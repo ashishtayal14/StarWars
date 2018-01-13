03f8647338907dca5cedb4a17a1fca2ca0ea2b0a
Ashish <ashishtayal14@gmail.com>
2018-01-14 01:21:26 +0530

    added the planetDetail component to display planed details

--- src/js/components/common/planet.jsx
+++ src/js/components/common/planet.jsx
@@ -1,29 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import PlanetDetail from './planetDetail';
 
-const Planet = (props) => {
-
-    const {
-        rotation_period,
-        name,
-        population,
-        orbital_period,
-        diameter
-} = props.planet
-    return (
-
-        <div className="col-md-10 planet" >
+class Planet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { showDetail: false };
+        this.showDetail = this.showDetail.bind(this);
+        this.hideDetail = this.hideDetail.bind(this);
+    }
+    showDetail() {
+        this.setState({ showDetail: true });
+    }
+    hideDetail() {
+        this.setState({ showDetail: false });
+    }
+    render() {
+        const {
+            rotation_period,
+            name,
+            population,
+            orbital_period,
+            diameter
+            } = this.props.planet
+        return (
             <div>
-                <h3><strong>{name}</strong></h3>
+                <div className="col-md-10 planet" onClick={this.showDetail}>
+                    <div>
+                        <h3><strong>{name}</strong></h3>
+                    </div>
+                    <span className="shadow"></span>                    
+                </div>
+                {this.state.showDetail && <PlanetDetail hideDetail={this.hideDetail} data={this.props.planet} />}
             </div>
-            <span className="shadow"></span>
-        </div>
-
-
-    )
+        );
+    }
 }
+
 Planet.propTypes = {
-    planet: PropTypes.object.isRequired,
+    planet: PropTypes.object.isRequired
 };
-export default Planet
+
+export default Planet;
--- src/js/components/home/home.jsx
+++ src/js/components/home/home.jsx
@@ -12,79 +12,86 @@ export default class Home extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            hasError:false,
-            isGuestUser:false
+            hasError: false,
+            isGuestUser: false
         }
         this.setSearchKey = this.setSearchKey.bind(this)
         this.filterPlanetSearch = this.filterPlanetSearch.bind(this);
-        if(!window.localStorage.getItem("status")){
+        if (!window.localStorage.getItem("status")) {
             this.props.history.push('/');
         }
         console.log(this.props);
     }
-    componentDidMount() {        
+    componentDidMount() {
         const status = JSON.parse(window.localStorage.getItem('status'))
-       
-        if(status == "login successfully"){
+
+        if (status == "login successfully") {
             const user = JSON.parse(window.localStorage.getItem('username'))
-            const isGuestUser  = user ? user.toLowerCase() !== "luke skywalker":""
-             this.setState({  isGuestUser:isGuestUser })
+            const isGuestUser = user ? user.toLowerCase() !== "luke skywalker" : ""
+            this.setState({ isGuestUser: isGuestUser })
         }
         this.props.actions.getDataRequest();
         this.props.actions.clearSearchKey();
     }
-   
-    setSearchKey(key){
+
+    showDetail(e) {
+        console.log(e.target);
+    }
+
+    setSearchKey(key) {
         this.props.actions.setSearchKey(key)
         this.props.actions.getFilteredDataRequest(key)
     }
-    filterPlanetSearch(filterText){        
+    filterPlanetSearch(filterText) {
         this.props.actions.setFilterKey(filterText);
         this.props.actions.filterPlanetSearch(filterText);
     }
     render() {
-    
-            return ( 
-                <PlanetDetails data={this.props} filterPlanetSearch = { this.filterPlanetSearch } setSearchKey = { this.setSearchKey } isGuestUser =  { this.state.isGuestUser }/>
-            )
-         
+
+        return (
+            <div>                
+                <PlanetList data={this.props} showDetail={ this.showDetail.bind(this) } filterPlanetSearch={this.filterPlanetSearch} setSearchKey={this.setSearchKey} isGuestUser={this.state.isGuestUser} />
+            </div>
+        )
+
     }
 }
 
-const PlanetDetails = (props) =>{
- const  {
-            actions, 
-            match, 
-            errorMessage, 
-            isFetching, 
-            planets,
-            filteredPlanets,
-            searchKey,
-            filterKey,
-            totalHits
-        } = props.data       
-        const { setSearchKey, isGuestUser, filterPlanetSearch } = props
-        const searchedPlanets = filterKey.length > 0 ? filteredPlanets : planets   
-return (                
-                <div className='component-block'>
-                    {isFetching && <Loader/>}
+const PlanetList = (props) => {
+    const {
+            actions,
+        match,
+        errorMessage,
+        isFetching,
+        planets,
+        filteredPlanets,
+        searchKey,
+        filterKey,
+        totalHits,
+        showDetail
+        } = props.data
+    const { setSearchKey, isGuestUser, filterPlanetSearch } = props
+    const searchedPlanets = filterKey.length > 0 ? filteredPlanets : planets
+    return (
+        <div className='component-block'>
+            {isFetching && <Loader />}
 
-                    <InputSearch setSearchKey={ setSearchKey } filterPlanetSearch = { filterPlanetSearch } searchKey={ searchKey } filterKey={filterKey} clearSearchKey={ actions.clearSearchKey } totalHits = { totalHits } isGuestUser = { isGuestUser  } />
-                                        
-                    {searchedPlanets && searchedPlanets.length > 0 && (
-                        <div className='col-md-12 alignGrid'>
-                            
-                            {
-                                _.map(searchedPlanets, (planet) => (
-                                    <Planet key={ planet.name } planet={ planet }/>
-                                ))
-                            }
-                        </div>
-                    )
-                    }                    
-                    { !isFetching && searchedPlanets && !searchedPlanets.length > 0 && <ErrorComponent className='error'  searchLimitExcedded = { totalHits > 14  && isGuestUser } noDataFound={ !searchedPlanets.length > 0 } />}
+            <InputSearch setSearchKey={setSearchKey} filterPlanetSearch={filterPlanetSearch} searchKey={searchKey} filterKey={filterKey} clearSearchKey={actions.clearSearchKey} totalHits={totalHits} isGuestUser={isGuestUser} />
+
+            {searchedPlanets && searchedPlanets.length > 0 && (
+                <div className='col-md-12 alignGrid'>
+
+                    {
+                        _.map(searchedPlanets, (planet) => (
+                            <Planet key={planet.name} planet={planet} showDetail={showDetail}/>
+                        ))
+                    }
                 </div>
-)
+            )
+            }
+            {!isFetching && searchedPlanets && !searchedPlanets.length > 0 && <ErrorComponent className='error' searchLimitExcedded={totalHits > 14 && isGuestUser} noDataFound={!searchedPlanets.length > 0} />}
+        </div>
+    )
 
 }
 
